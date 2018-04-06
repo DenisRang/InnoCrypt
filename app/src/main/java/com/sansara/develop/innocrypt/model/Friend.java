04b0c0e39d1fd37c0372ef68974eac25dada8484
denisrang <den41998@mail.ru>
2018-04-06 15:21:28 +0300

    Initial commit

---
+++ app/src/main/java/com/sansara/develop/innocrypt/model/Friend.java
@@ -0,0 +1,8 @@
+package com.sansara.develop.innocrypt.model;
+
+
+
+public class Friend extends User{
+    public String id;
+    public String idRoom;
+}
---
+++ app/src/main/java/com/sansara/develop/innocrypt/model/Status.java
@@ -0,0 +1,13 @@
+package com.sansara.develop.innocrypt.model;
+
+
+
+public class Status{
+    public boolean isOnline;
+    public long timestamp;
+
+    public Status(){
+        isOnline = false;
+        timestamp = 0;
+    }
+}
